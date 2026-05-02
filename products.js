aeeca596bcfe3855b550d67f459b3e49ed896c7c
bida25-434 <bida25-434@thuto.bac.ac.bw>
2026-05-02 22:52:43 +0200

    Delete products.js

--- products.js
+++
@@ -1,9 +0,0 @@
-*{
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-function addToCart(name, price) {
-  cart.push({ name, price });
-  localStorage.setItem("cart", JSON.stringify(cart));
-
-  alert(name + " added to cart!");
-}
